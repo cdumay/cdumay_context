a0f4783f37e2958a49d1c722bae5979e8b4f6e3b
Cedric dumay <cedric.dumay@gmail.com>
2025-02-05 23:55:07 +0100

    build: renamed package

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! [![License: BSD-3-Clause](https://img.shields.io/badge/license-BSD--3--Clause-blue)](./LICENSE)
-//! [![cdumay-context on crates.io](https://img.shields.io/crates/v/cdumay-context)](https://crates.io/crates/cdumay-context)
-//! [![cdumay-context on docs.rs](https://docs.rs/cdumay-context/badge.svg)](https://docs.rs/cdumay-context)
+//! [![cdumay_context on crates.io](https://img.shields.io/crates/v/cdumay_context)](https://crates.io/crates/cdumay_context)
+//! [![cdumay_context on docs.rs](https://docs.rs/cdumay_context/badge.svg)](https://docs.rs/cdumay_context)
 //! [![Source Code Repository](https://img.shields.io/badge/Code-On%20GitHub-blue?logo=GitHub)](https://github.com/cdumay/cdumay_context)
 //!
-//! `cdumay-context` is a lightweight and efficient Rust library designed for manipulating a context and exporting it into various formats. The library
+//! `cdumay_context` is a lightweight and efficient Rust library designed for manipulating a context and exporting it into various formats. The library
 //! provides simple methods to handle structured data and export it in widely used formats like `JSON`, `TOML`, and `YAML`.
 //!
 //! This makes it an ideal tool for developers working with configuration management, data serialization, or any use case requiring flexible
@@ -16,16 +16,16 @@
 //!
 //! # Usage
 //!
-//! To utilize `cdumay-context` in your project, follow these steps:
+//! To utilize `cdumay_context` in your project, follow these steps:
 //!
-//! 1. **Add Dependencies**: To use `cdumay-context` in your project, add it to your Cargo.toml as a dependency:
+//! 1. **Add Dependencies**: To use `cdumay_context` in your project, add it to your Cargo.toml as a dependency:
 //!
 //! ```toml
 //! [dependencies]
-//! cdumay-context = "0.1"
+//! cdumay_context = "0.1"
 //! ```
 //!
-//! 2. **Define Context**: The core feature of `cdumay-context` is the context. The context acts as a container where you can store key-value pairs of data.
+//! 2. **Define Context**: The core feature of `cdumay_context` is the context. The context acts as a container where you can store key-value pairs of data.
 //! Here's how to create and manipulate it:
 //!
 //! ```rust
@@ -45,7 +45,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! cdumay-context = {version = "0.1", features = ["json"] }
+//! cdumay-context = {version = "1.0", features = ["json"] }
 //! ```
 //!
 //! ```rust
